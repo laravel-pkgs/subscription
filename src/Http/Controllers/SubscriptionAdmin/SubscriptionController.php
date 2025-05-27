4bc53578479634a914b57c792705dfe921f3c4a0
Alireza <dev.azizimail@gmail.com>
2025-05-27 12:32:26 +0330

    add count of subscription

--- src/Http/Controllers/SubscriptionAdmin/SubscriptionController.php
+++ src/Http/Controllers/SubscriptionAdmin/SubscriptionController.php
@@ -3,7 +3,9 @@
 namespace IICN\Subscription\Http\Controllers\SubscriptionAdmin;
 
 use IICN\Subscription\Http\Controllers\Controller;
+use IICN\Subscription\Models\SubscriptionTransaction;
 use IICN\Subscription\Models\SubscriptionUser;
+use Illuminate\Support\Facades\DB;
 use Modules\Common\Http\Responses\GenericResponse;
 use IICN\Subscription\Http\Resources\SubscriptionUserResources;
 use Spatie\QueryBuilder\AllowedFilter;
@@ -23,5 +25,52 @@ class SubscriptionController extends Controller
             ->paginate();
 
         return GenericResponse::success(data: SubscriptionUserResources::collection($subscriptionUser));
+    }
+
+    public function userSubscriptionGroupedByMonth()
+    {
+        $now = now();
+        $successStatus = 'success';
+
+        return [
+            'today' => SubscriptionTransaction::query()
+                ->with(['subscription'])
+                ->select('subscription_id', DB::raw('COUNT(*) as count'))
+                ->where('status', $successStatus)
+                ->whereDate('created_at', $now->toDateString())
+                ->groupBy('subscription_id')
+                ->get(),
+
+            'last_7_days' => SubscriptionTransaction::query()
+                ->with('subscription')
+                ->select('subscription_id', DB::raw('COUNT(*) as count'))
+                ->where('status', $successStatus)
+                ->whereBetween('created_at', [$now->copy()->subDays(6)->startOfDay(), $now])
+                ->groupBy('subscription_id')
+                ->get(),
+
+            'last_month' => SubscriptionTransaction::query()
+                ->with('subscription')
+                ->select('subscription_id', DB::raw('COUNT(*) as count'))
+                ->where('status', $successStatus)
+                ->whereBetween('created_at', [$now->copy()->subMonth()->startOfMonth(), $now->copy()->subMonth()->endOfMonth()])
+                ->groupBy('subscription_id')
+                ->get(),
+
+            'last_year' => SubscriptionTransaction::query()
+                ->with('subscription')
+                ->select('subscription_id', DB::raw('COUNT(*) as count'))
+                ->where('status', $successStatus)
+                ->whereYear('created_at', $now->subYear()->year)
+                ->groupBy('subscription_id')
+                ->get(),
+
+            'total' => SubscriptionTransaction::query()
+                ->with('subscription')
+                ->select('subscription_id', DB::raw('COUNT(*) as count'))
+                ->where('status', $successStatus)
+                ->groupBy('subscription_id')
+                ->get(),
+        ];
     }
 }
